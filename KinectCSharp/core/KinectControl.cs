4ab4e6dcbb3fe3c305061e662bed350b56bcc832
maoyiwei <jsmyw95@qq.com>
2018-02-22 14:19:16 +0800

    feature

--- KinectCSharp/core/KinectControl.cs
+++ KinectCSharp/core/KinectControl.cs
@@ -7,20 +7,72 @@ using System.Threading.Tasks;
 namespace KinectCSharp.core
 {
     using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
     using System.IO;
+    using System.Collections;
+    using KinectCSharp.util;
+
 
     class KinectControl
     {
         private KinectSensor sensor;
+        private bool record = false;
+        private static bool sensorOpen = false;
+
+        public List<Feature> featureBuffer;
+
+        public delegate void FeatureReady(Feature feature);
+
 
         public KinectControl()
         {
-            this.Initialize();
         }
 
-        // 初始化Kinect
-        private void Initialize()
+        // 收到一个帧的事件处理函数
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
+            Skeleton[] skeletons = new Skeleton[0];
+            Feature feature = new Feature();
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+            // 如果开启录制
+            if (this.record)
+            {
+                this.featureBuffer.Add(feature);
+            }
+        }
+
+        // 开始存储到Buffer
+        public bool openBuffer()
+        {
+            this.record = true;
+            return this.record;
+        }
+
+        // 停止存储到Buffer
+        public bool stopBuffer()
+        {
+            this.record = false;
+            return this.record;
+        }
+
+        // 初始化Kinect设备
+        private void InitializeFaculty()
+        {
+            // 确保设备只会被启动一次
+            if (KinectControl.sensorOpen)
+            {
+                return;
+            }
+            KinectControl.sensorOpen = true;
+
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
                 if (potentialSensor.Status == KinectStatus.Connected)
@@ -32,7 +84,7 @@ namespace KinectCSharp.core
                 {
                     // Turn on the skeleton stream to receive skeleton frames
                     this.sensor.SkeletonStream.Enable();
-
+                    this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
 
                     // Start the sensor!
                     try
@@ -46,5 +98,34 @@ namespace KinectCSharp.core
                 }
             }
         }
+
+        // TODO 从文件加载
+        public void loadFromFile(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+
+            fs.Close();
+            Console.WriteLine("文件读取成功");
+        }
+
+        // TODO 保存到文件
+        public void saveToFile(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Create);
+            for(int i = 0;i < featureBuffer.Count; i++)
+            {
+                byte[] btData = Util.ObjectToBytes(featureBuffer[i]);
+                fs.Write(btData, 0, btData.Length);
+            }
+            fs.Close();
+            Console.WriteLine("文件保存成功");
+        }
+
+        // 清空缓存
+        public void emptyBuffer()
+        {
+            this.featureBuffer.Clear();
+        }
+
     }
 }
